24a5999ca04849e6c9ffabeb09c38df430cd6fd9
Nicolas Bender <nico.newz@posteo.de>
2021-05-23 21:29:38 +0200

    Added custom Exceptions

---
+++ src/main/java/landing/exceptions/BadParenthesisException.java
@@ -0,0 +1,7 @@
+package landing.exceptions;
+
+public class BadParenthesisException extends Exception {
+    public BadParenthesisException(String errorMessage) {
+        super(errorMessage);
+    }
+}
---
+++ src/main/java/landing/exceptions/BadSymbolException.java
@@ -0,0 +1,7 @@
+package landing.exceptions;
+
+public class BadSymbolException extends Exception{
+    public BadSymbolException(String errorMessage) {
+        super(errorMessage);
+    }
+}
