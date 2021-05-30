51ec631e20ae5e0512c26351ad4896ef6b52a260
Nicolas Bender <nico.newz@posteo.de>
2021-05-30 15:53:13 +0200

    Split Tests

--- src/test/java/landing/l2/applicationCode/scanner/NumberBuilderTest.java
+++ src/test/java/landing/l2/applicationCode/scanner/NumberBuilderTest.java
@@ -8,49 +8,89 @@ import static org.junit.Assert.assertThrows;
 
 public class NumberBuilderTest {
     @Test
-    public void testDetermineStringForValueFrom() {
+    public void testDetermineStringForValueFromInteger() {
         String correctInteger = "12345$";
-        String correctDouble = "123.45$";
-        String correctDecimalPoint = ".$";
-        String correctZeroBefore = "0.$";
-        String correctZeroAfter = ".0$";
-        String wrongDecimalPoint0 = "1.2.3$";
-        String wrongDecimalPoint1 = "1..2$";
-        String wrongDecimalPoint2 = "12..34$";
-        String wrongDecimalPoint3 = "..$";
-
         String answerToCorrectInteger = NumberBuilder.determineStringForValueFrom(correctInteger) + "$";
-        String answerToCorrectDouble = NumberBuilder.determineStringForValueFrom(correctDouble) + "$";
-        String answerToCorrectDecimalPoint = NumberBuilder.determineStringForValueFrom(correctDecimalPoint) + "$";
-        String answerToCorrectZeroBefore = NumberBuilder.determineStringForValueFrom(correctZeroBefore) + "$";
-        String answerToCorrectZeroAfter = NumberBuilder.determineStringForValueFrom(correctZeroAfter) + "$";
-
         assertEquals(correctInteger, answerToCorrectInteger);
+    }
+
+    @Test
+    public void testDetermineStringForValueFromDouble() {
+        String correctDouble = "123.45$";
+        String answerToCorrectDouble = NumberBuilder.determineStringForValueFrom(correctDouble) + "$";
         assertEquals(correctDouble, answerToCorrectDouble);
+    }
+
+    @Test
+    public void testDetermineStringForValueFromDecimalPoint() {
+        String correctDecimalPoint = ".$";
+        String answerToCorrectDecimalPoint = NumberBuilder.determineStringForValueFrom(correctDecimalPoint) + "$";
         assertEquals(correctDecimalPoint, answerToCorrectDecimalPoint);
+    }
+
+    @Test
+    public void testDetermineStringForValueFromDecimalPointAfter() {
+        String correctZeroBefore = "0.$";
+        String answerToCorrectZeroBefore = NumberBuilder.determineStringForValueFrom(correctZeroBefore) + "$";
         assertEquals(correctZeroBefore, answerToCorrectZeroBefore);
+    }
+
+    @Test
+    public void testDetermineStringForValueFromDecimalPointBefore() {
+        String correctZeroAfter = ".0$";
+        String answerToCorrectZeroAfter = NumberBuilder.determineStringForValueFrom(correctZeroAfter) + "$";
         assertEquals(correctZeroAfter, answerToCorrectZeroAfter);
+    }
+
+    @Test
+    public void testDetermineStringForValueFromWrongDecimalPoint0() {
+        String wrongDecimalPoint0 = "1.2.3$";
         assertThrows(NumberFormatException.class, () -> NumberBuilder.determineStringForValueFrom(wrongDecimalPoint0));
+    }
+
+    @Test
+    public void testDetermineStringForValueFromWrongDecimalPoint1() {
+        String wrongDecimalPoint1 = "1..2$";
         assertThrows(NumberFormatException.class, () -> NumberBuilder.determineStringForValueFrom(wrongDecimalPoint1));
+    }
+
+    @Test
+    public void testDetermineStringForValueFromWrongDecimalPoint2() {
+        String wrongDecimalPoint2 = "12..34$";
         assertThrows(NumberFormatException.class, () -> NumberBuilder.determineStringForValueFrom(wrongDecimalPoint2));
+    }
+
+    @Test
+    public void testDetermineStringForValueFromWrongDecimalPoint3() {
+        String wrongDecimalPoint3 = "..$";
         assertThrows(NumberFormatException.class, () -> NumberBuilder.determineStringForValueFrom(wrongDecimalPoint3));
     }
 
     @Test
-    public void testConvertValueToDoubleFrom() {
+    public void testConvertValueToDoubleFromInteger() {
         double correctInteger = 12345;
-        double correctDouble = 123.45;
-        double correctZeroBefore = 0.;
-        double correctZeroAfter = .0;
-
         double answerToCorrectInteger = NumberBuilder.convertValueToDoubleFrom("12345");
-        double answerToCorrectDouble = NumberBuilder.convertValueToDoubleFrom("123.45");
-        double answerToCorrectZeroBefore = NumberBuilder.convertValueToDoubleFrom("0.");
-        double answerToCorrectZeroAfter = NumberBuilder.convertValueToDoubleFrom(".0");
-
         assertEquals(correctInteger, answerToCorrectInteger, 1E-6);
+    }
+
+    @Test
+    public void testConvertValueToDoubleFromDouble() {
+        double correctDouble = 123.45;
+        double answerToCorrectDouble = NumberBuilder.convertValueToDoubleFrom("123.45");
         assertEquals(correctDouble, answerToCorrectDouble, 1E-6);
+    }
+
+    @Test
+    public void testConvertValueToDoubleFromDecimalPointAfter() {
+        double correctZeroBefore = 0.;
+        double answerToCorrectZeroBefore = NumberBuilder.convertValueToDoubleFrom("0.");
         assertEquals(correctZeroBefore, answerToCorrectZeroBefore, 1E-6);
+    }
+
+    @Test
+    public void testConvertValueToDoubleFromDecimalPointBefore() {
+        double correctZeroAfter = .0;
+        double answerToCorrectZeroAfter = NumberBuilder.convertValueToDoubleFrom(".0");
         assertEquals(correctZeroAfter, answerToCorrectZeroAfter, 1E-6);
     }
 }
